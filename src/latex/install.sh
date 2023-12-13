6e677181a9a26e2593f3428d0c7e90fb55775ce2
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-12-13 02:11:09 +0000

    optimized installation and testing

--- src/latex/install.sh
+++ src/latex/install.sh
@@ -2,11 +2,21 @@
 set -e
 
 
-SCHEME=${SCHEME:-"full"}
+SCHEME=${SCHEME:-"basic"}
 PACKAGES=${PACKAGES:-""}
 
-apt update
-apt install -y wget perl-modules libfontconfig1 fontconfig
+check_packages() {
+	if ! dpkg -s "$@" >/dev/null 2>&1; then
+		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+			echo "Running apt-get update..."
+			apt-get update -y
+		fi
+		apt-get -y install --no-install-recommends "$@"
+	fi
+}
+
+
+check_packages ca-certificates perl wget perl-modules libfontconfig1 fontconfig
 
 cd /tmp # working directory of your choice
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz # or curl instead of wget
@@ -20,13 +30,20 @@ TEXLIVE_EXECUTABLES_DIR="$(ls -d $TEXLIVE_DIR/bin/*)"
 # Install latex and latexmk
 $TEXLIVE_EXECUTABLES_DIR/tlmgr install latex latex-bin latexmk
 
-# Split $PACKAGES string into separate strings using comma and/or space as delimiter and install them
-for PACKAGE in $(echo $PACKAGES | tr ', ' '\n'); do
-    $TEXLIVE_EXECUTABLES_DIR/tlmgr install $PACKAGE
-done
+# Split $PACKAGES string into separate strings using comma as delimiter, trim the resulting strings after split, and install them
+echo $PACKAGES | tr ',' '\n' | xargs -I % sh -c "$TEXLIVE_EXECUTABLES_DIR/tlmgr install %"
 
 
 chmod -R 777 /usr/local/texlive/
 
 echo "PATH=\"\$PATH:$TEXLIVE_EXECUTABLES_DIR\"" >> /etc/environment
 ls $TEXLIVE_EXECUTABLES_DIR | xargs -I % ln -s $TEXLIVE_EXECUTABLES_DIR/% /usr/local/bin/%
+
+# cleanup
+
+rm -rf /var/lib/apt/lists/*
+cd /tmp
+rm -rf $(ls -d */ | grep install-tl-)
+rm -rf install-tl-unx.tar.gz
+
+echo "Done!"
