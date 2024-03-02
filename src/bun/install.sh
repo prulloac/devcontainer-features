483ec77ce17235264e59f88323af75ddbc01568a
Pablo Ulloa <pablo.ulloac@usach.cl>
2024-03-02 22:53:21 +0000

    updated bun installation

--- src/bun/install.sh
+++ src/bun/install.sh
@@ -2,23 +2,22 @@
 
 set -e
 
-BUN_VERSION="${VERSION:-"latest"}"
+check_packages() {
+	if ! dpkg -s "$@" >/dev/null 2>&1; then
+		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+			echo "Running apt-get update..."
+			apt-get update -y
+		fi
+		apt-get -y install --no-install-recommends "$@"
+	fi
+}
 
-source ./library_scripts.sh
+check_packages ca-certificates curl unzip
 
-# nanolayer is a cli utility which keeps container layers as small as possible
-# source code: https://github.com/devcontainers-contrib/nanolayer
-# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-# and if missing - will download a temporary copy that automatically get deleted at the end 
-# of the script
-ensure_nanolayer nanolayer_location "v0.5.6"
+curl -fsSL https://bun.sh/install | bash 
 
-
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers/features/node:1"
-
-npm install --global bun@$BUN_VERSION
+echo '# bun' >> /etc/profile.d/bun.sh
+echo 'export BUN_INSTALL="$HOME/.bun"' >> /etc/profile.d/bun.sh
+echo 'export PATH=$BUN_INSTALL/bin:$PATH' >> /etc/profile.d/bun.sh
 
 echo 'Done!'
